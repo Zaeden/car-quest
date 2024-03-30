02973e35e8d2203411c50d3dad168b67c1f860d7
Mizan Ali Khan <mizanpathan3@gmail.com>
2024-03-30 14:19:37 +0530

    deploying on render.com

--- client/svelte.config.js
+++ client/svelte.config.js
@@ -1,11 +1,26 @@
-import adapter from "@sveltejs/adapter-auto";
+// import adapter from "@sveltejs/adapter-auto";
+// import { vitePreprocess } from "@sveltejs/vite-plugin-svelte";
+// /** @type {import('@sveltejs/kit').Config} */
+// const config = {
+//   kit: {
+//     adapter: adapter(),
+//   },
+//   preprocess: vitePreprocess(),
+//   ssr: false
+// };
+// export default config;
+
+import node from "@sveltejs/adapter-node";
 import { vitePreprocess } from "@sveltejs/vite-plugin-svelte";
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
   kit: {
-    adapter: adapter(),
+    adapter: node(),
+    methodOverride: {
+      allowed: ["PATCH", "DELETE"],
+    },
   },
   preprocess: vitePreprocess(),
-  ssr: false
+  ssr: false,
 };
 export default config;
